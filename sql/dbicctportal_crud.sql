ee7b532bb52b6f00650deaf0250ea2506d84e6b5
ivancotacte <cotactearmenion@gmail.com>
2024-03-20 21:36:02 +0800

    Add course and contact number fields to tb_account table

--- sql/dbicctportal_crud.sql
+++ sql/dbicctportal_crud.sql
@@ -8,7 +8,9 @@ CREATE TABLE `tb_account` (
   `middle_name` varchar(50) NOT NULL,
   `last_name` varchar(50) NOT NULL,
   `suffix_name` varchar(50) NOT NULL,
+  `course` varchar(50) NOT NULL,
   `campus` varchar(50) NOT NULL,
+  `contact_number` varchar(50) NOT NULL,
   `email_address` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   `user_role` varchar(50) NOT NULL DEFAULT 'student',
@@ -16,3 +18,6 @@ CREATE TABLE `tb_account` (
   `created_at` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+INSERT INTO tb_account (first_name, email_address, password, user_role) VALUES ('admin', 'admin@admin.com', 'admin', 'admin');
