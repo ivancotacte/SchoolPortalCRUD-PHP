6950deb023aa426bd9d038797626ad79dceead7b
ivancotacte <cotactearmenion@gmail.com>
2024-03-20 22:44:14 +0800

    Update database connection configuration

--- config/connect.php
+++ config/connect.php
@@ -1,8 +1,14 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "dbicctportal_crud";
+
+require_once realpath(__DIR__ . "/../vendor/autoload.php");
+use Dotenv\Dotenv;
+$dotenv = Dotenv::createImmutable(__DIR__ . "/../");
+$dotenv->load();
+
+$servername = $_ENV['DATABASE_HOST'];
+$username = $_ENV['DATABASE_USER'];
+$password = $_ENV['DATABASE_PASSWORD'];
+$dbname = $_ENV['DATABASE_NAME'];
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
