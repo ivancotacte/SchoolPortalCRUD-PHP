d4e470605238f4d5883317f5a464f0f47bfc856d
ivancotacte <cotactearmenion@gmail.com>
2024-03-21 21:47:27 +0800

    Set default timezone to Asia/Manila

--- config/connect.php
+++ config/connect.php
@@ -1,5 +1,5 @@
 <?php
-
+date_default_timezone_set('Asia/Manila');
 require_once realpath(__DIR__ . "/../vendor/autoload.php");
 use Dotenv\Dotenv;
 $dotenv = Dotenv::createImmutable(__DIR__ . "/../");
