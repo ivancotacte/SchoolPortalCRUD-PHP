4313ac4b4e431e6f97a9cbc80d213da2cadfaa78
ivancotacte <cotactearmenion@gmail.com>
2024-04-02 15:46:00 +0800

    Update admin-dashboard.php with new styles for col class

--- admin-dashboard.php
+++ admin-dashboard.php
@@ -40,11 +40,17 @@ if (isset($_POST['delete'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
 </head>
+<style>
+    .col {
+        margin-top: 50px;
+        weight: 100%;
+    }
+</style>
 <body>
 
     <div class="container d-flex justify-content-center align-items-center">
         <div class="row justify-content-center">
-            <div class="col-md-1000">
+            <div class="col">
                 <div class="p-3 border rounded-2 bg-white shadow">
                     <h2 class="mb-4">Student Accounts</h2>
                     <button type="button" onclick="window.location.href = 'add_student.php';" class="btn btn-primary"> Add Student </button>
